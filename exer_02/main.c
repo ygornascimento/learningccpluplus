bc1a54bdf0ab0da235f984d69a249263a9664972
Ygor Nascimento <ygor.nascimento@outlook.com>
2026-06-05 18:45:00 -0300

    Exer_02

---
+++ exer_02/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero_inteiro;
+
+    printf("Escreva um número inteiro: ");
+    scanf("%d", &numero_inteiro);
+    printf("Número digitado: %d", numero_inteiro);
+
+
+    return 0;
+}
